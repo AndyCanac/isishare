67e831442699444524e0e7f9913bc4c7834cb665
Andyisitech <andy.canac@ecole-isitech.fr>
2024-02-02 12:03:09 +0100

    [UPDATE] - Error page, components & main page

--- isishare/src/app/page.tsx
+++ isishare/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Head from "@/components/Head"
 import Footer from "@/components/Footer"
+import Hero from "@/components/Hero"
 
 export default function Home() {
   return (
     <>
       <Head />
+      <Hero />
       <Footer />
     </>
   )
--- isishare/src/components/Footer.tsx
+++ isishare/src/components/Footer.tsx
@@ -1,28 +1,22 @@
 export default function Footer(){
     return (
-<footer className="bg-white">
-    <div className="container px-6 py-8 mx-auto">
-        <div className="flex flex-col items-center text-center">
+<footer className="flex justify-center px-4 text-gray-800 bg-white dark:text-white">
+    <div className="container px-6 py-6">
+        <hr className="h-px bg-gray-200 border-none my-7 dark:bg-gray-700"/>
+
+        <div className="flex flex-col items-center justify-between md:flex-row">
             <a href="#">
-                <img className="w-auto h-10" src="/isishare.png" alt=""/>
+                <img className="w-auto h-14" src="/isishare.png"/>
             </a>
 
-            <div className="flex flex-wrap justify-center mt-6 -mx-4">
-                <a href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Lorem </a>
-                
-                <a href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Lorem </a>
-                
-                <a href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Lorem </a>
-
-                <a href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Lorem </a>
+            <div className="flex mt-4 md:m-0">
+                <div className="-mx-4">
+                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">About</a>
+                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">Blog</a>
+                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">News</a>
+                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">Contact</a>
+                </div>
             </div>
-
-        </div>
-
-        <hr className="my-6 border-gray-200 md:my-10 dark:border-gray-700" />
-
-        <div className="flex flex-col items-center sm:flex-row sm:justify-between">
-            <p className="text-sm text-gray-500 dark:text-gray-300">© Copyright 2024.</p>
         </div>
     </div>
 </footer>
