a7bfe6820583d01a564490248c9e7269b4d620c7
ToadyGamer <castellierflorian@gmail.com>
2024-02-21 14:20:20 +0100

    [ADD] - FrontEnd of the Profile Page
    page : /profile

---
+++ isishare/src/components/Profile.tsx
@@ -0,0 +1,50 @@
+export default function Profile() {
+    return (
+    <div className="ml-12 w-1/5 max-w-sm overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 float-right">
+        <img
+        className="object-cover object-center w-full h-56"
+        src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+        alt="avatar"/>
+
+        <div className="flex items-center px-6 py-3 bg-gray-900">
+            <h1 className="mx-3 text-lg font-semibold text-white">Nom / Prenom</h1>
+        </div>
+
+        <div className="px-6 py-4">
+            <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                    <img className="object-cover w-10 h-10" src="Logo/DiscordLogo.png"></img>
+                <h1 className="px-2 text-sm">Discord</h1>
+            </div>
+            <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                    <img className="object-cover w-10 h-10" src="Logo/InstagramLogo.png"></img>
+                <h1 className="px-2 text-sm">Instagram</h1>
+            </div>
+            <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                    <img className="object-cover w-10 h-10" src="Logo/LinkedinLogo.png"></img>
+                <h1 className="px-2 text-sm">Linkedin</h1>
+            </div>
+            <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                    <img className="object-cover w-10 h-10" src="Logo/MailLogo.png"></img>
+                <h1 className="px-2 text-sm">Mail</h1>
+            </div>
+            <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                    <img className="object-cover w-10 h-10" src="Logo/TeamsLogo.png"></img>
+                <h1 className="px-2 text-sm">Teams</h1>
+            </div>
+            <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                    <img className="object-cover w-10 h-10" src="Logo/TelephoneLogo.png"></img>
+                <h1 className="px-2 text-sm">Telephone</h1>
+            </div>
+            <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                    <img className="object-cover w-10 h-10" src='Logo/XLogo.png'></img>
+                <h1 className="px-2 text-sm">X</h1>
+                <button className="ml-20 px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">-</button>
+            </div>
+
+            <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add contact</button>
+            </div>
+        </div>
+    </div>
+    )
+}
---
+++ isishare/src/components/WTL.tsx
@@ -0,0 +1,115 @@
+export default function WTL() {
+    return (
+        <section className="container px-4 mx-auto">
+        <div className="flex flex-col">
+        <h1 className="text-center mt-10 mb-3 text-3xl">Want to learn</h1>
+            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                    <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                        <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                            <thead className="bg-gray-50 dark:bg-gray-800">
+                                <tr>
+                                    <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                        Connaissance
+                                    </th>
+                                    <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                        Description
+                                    </th>
+                                    <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                                <tr>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">C#</td>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Une superbe description par rapport à mon projet</td>
+                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                        <div className="flex items-center gap-x-6">
+                                            <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                                Remove
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">C#</td>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Une superbe description par rapport à mon projet</td>
+                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                        <div className="flex items-center gap-x-6">
+                                            <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                                Remove
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">C#</td>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Une superbe description par rapport à mon projet</td>
+                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                        <div className="flex items-center gap-x-6">
+                                            <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                                Remove
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">C#</td>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Une superbe description par rapport à mon projet</td>
+                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                        <div className="flex items-center gap-x-6">
+                                            <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                                Remove
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">C#</td>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Une superbe description par rapport à mon projet</td>
+                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                        <div className="flex items-center gap-x-6">
+                                            <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                                Remove
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">C#</td>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Une superbe description par rapport à mon projet</td>
+                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                        <div className="flex items-center gap-x-6">
+                                            <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                                Remove
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Nom du programme</td>
+                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Description de ce que vous voulez apprendre</td>
+                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                        <div className="flex items-center gap-x-6">
+                                            <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                                Add
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
